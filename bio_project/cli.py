d6d1d3fc2215873c65bc520d95bc51c78d143d54
Federico Baldo <federicobaldo@Federicos-MacBook-Pro.local>
2019-02-10 14:16:57 +0100

    added human readable output

--- bio_project/cli.py
+++ bio_project/cli.py
@@ -122,6 +122,11 @@ centers = compute_geometry.center_mass_unit(answers['draw'], unit_dir)
 if answers['center_of_mass']:
     if answers['json']:
         print(json.dumps(centers, indent=4, sort_keys=True))
+    else:
+        for unit in unit_dir_list:
+            keys = natural_keys(unit)
+            print("Center of mass of " + unit.split('.')[0] + " is: " +
+                  str(centers[unit]))
 
 if answers['center_of_mass_distance']:
     ris = compute_geometry.distance_center_of_mass(centers,
@@ -129,29 +134,75 @@ if answers['center_of_mass_distance']:
                                                    unit_dir)
     if answers['json']: 
         print(json.dumps(ris, indent=4, sort_keys=True)) 
+    else:
+        for unit_1 in unit_dir_list:
+            for unit_2 in unit_dir_list:
+                if unit_1 != unit_2:
+                    print("The distance between the center of mass of" + 
+                          unit_1.split('.')[0] + " and " +
+                          unit_2.split('.')[0] + " is: " + 
+                          str(ris[unit_1][unit_2])) 
 
 if answers['c_alpha_distance']:
     ris = compute_geometry.distance_alpha_c(centers, answers['draw'], unit_dir)
     if answers['json']:
         print(json.dumps(ris, indent=4, sort_keys=True)) 
+    else:
+        for unit in unit_dir_list:
+            for ca in ris[unit]:
+                dist = str(ris[unit][ca])
+                print("The distance between " + unit.split('.')[0] + 
+                      " and alpha carbon " + str(natural_keys(ca)[1]) + 
+                      " is: " + dist)
 
 if answers['handedness']:
     ris = compute_geometry.handedness(centers, answers['draw'], unit_dir)
     if answers['json']:
         print(json.dumps(ris, indent=4, sort_keys=True), unit_dir) 
+    else:
+        for unit in unit_dir_list:
+            keys = natural_keys(unit)
+            hand = 'left-handed' if ris[unit] == 'L' else 'right-handed'
+            print("The " + unit.split('.')[0] + " is " + hand)
 
 if answers['twist']:
     ris = compute_geometry.twist(centers, answers['draw'], unit_dir)
     if answers['json']:
         print(json.dumps(ris, indent=4, sort_keys=True))
+    else:
+        for i in range(len(unit_dir_list)-1):
+            unit_1 = unit_dir_list[i]
+            unit_2 = unit_dir_list[i+1]
+            twist = str(ris[unit_1 + '_' + unit_2])
+            print("The twist between " + 
+                  unit_1.split('.')[0] + " and " +
+                  unit_2.split('.')[0] + " is: " + 
+                  twist)
 
 if answers['curvature']:
     ris = compute_geometry.curvature(centers, answers['draw'], unit_dir)
     if answers['json']:
         print(json.dumps(ris, indent=4, sort_keys=True))
+    else:
+        for i in range(len(unit_dir_list)-2):
+            unit_1 = unit_dir_list[i]
+            unit_2 = unit_dir_list[i+1]
+            curv = str(ris[unit_1 + '_' + unit_2])
+            print("The curvature between " + 
+                  unit_1.split('.')[0] + " and " +
+                  unit_2.split('.')[0] + " is: " + 
+                  curv)
 
 if answers['pitch']: 
     ris = compute_geometry.pitch(centers, answers['draw'], unit_dir)
     if answers['json']:
         print(json.dumps(ris, indent=4, sort_keys=True)) 
-
+    else:
+        for i in range(len(unit_dir_list)-2):
+            unit_1 = unit_dir_list[i]
+            unit_2 = unit_dir_list[i+1]
+            pitch = str(ris[unit_1 + '_' + unit_2])
+            print("The pitch between " + 
+                  unit_1.split('.')[0] + " and " +
+                  unit_2.split('.')[0] + " is: " + 
+                  pitch)
